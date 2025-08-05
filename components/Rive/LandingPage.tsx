61052176f816a0a89f2e1b91748ad5b501977ebe
vgugnani <vanshajgugnani04@gmail.com>
2025-08-04 21:11:53 -0400

    phone fix

--- components/Rive/LandingPage.tsx
+++ components/Rive/LandingPage.tsx
@@ -10,14 +10,14 @@ export default function LandingVehicle() {
   });
 
   return (
-    <div className="fixed inset-0 flex">
+    <div className=" flex">
       {/* Left side animation - hidden on mobile, 40% width */}
       <div className="hidden md:flex w-2/5 h-screen justify-center items-end">
         <Animation style={{ width: "80%", height: "80%" }} />
       </div>
       
       {/* Right side content - full width on mobile, 60% on desktop */}
-      <div className="w-full md:w-3/5 h-full">
+      <div className="w-full md:w-3/5 ">
         <BookMe />
       </div>
     </div>
